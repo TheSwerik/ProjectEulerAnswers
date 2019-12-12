fafe1fc574d6ce17b929da66a76669dae984c5bf
TheSwerik <derswerik@gmail.com>
2019-12-12 14:40:45 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import till100.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nWhich do you want to try out?");
+            int input = sc.nextInt();
+            if (input == 0) {
+                System.exit(0);
+            }
+            try {
+                Class.forName("till100.Problem" + input).newInstance();
+            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                System.out.println("not valid\n");
+                e.printStackTrace();
+            }
+        }
+    }
+}
