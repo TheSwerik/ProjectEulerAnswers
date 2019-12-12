8ed39135be88d525b77e963000e0749970978254
TheSwerik <derswerik@gmail.com>
2019-12-12 15:38:38 +0100

    Problem9

--- src/till10/Problem9.java
+++ src/till10/Problem9.java
@@ -6,7 +6,19 @@ public class Problem9 {
         long result = 0;
 
         // Solution:
-
+        outer:
+        for (int i = 0; i < 500; i++) {
+            for (int j = 0; j < 500; j++) {
+                for (int k = 0; k < 500; k++) {
+                    if (i * i + j * j == k * k && i + j + k == 1000) {
+                        result = i * j * k;
+                        break outer;
+                    }
+                }
+            }
+        }
+        // was ultra easy (?)
+        //  20 or 45 ms
 
         long timeToResolve = System.nanoTime() - startTime;
         if (((double) timeToResolve / 1_000_000) >= 1000) {
