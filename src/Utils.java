b5625d12945e30f477c65e21a7b40dcfcc0089a5
TheSwerik <derswerik@gmail.com>
2019-12-12 16:49:18 +0100

    faster 7

--- src/Utils.java
+++ src/Utils.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
     public int findBiggestPrimFactor(long input) {
         int i;
@@ -10,6 +13,23 @@ public class Utils {
             }
         }
         return i;
+    }
+
+    public int genPrimes(int range) {
+        ArrayList<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        outer:
+        for (int i = 3; primes.size() < range; i += 2) {
+            int sqrt = (int) Math.sqrt(i);
+            for (int j = 0; j < primes.size() && primes.get(j) <= sqrt; j++) {
+                if (i % primes.get(j) == 0) {
+                    continue outer;
+                }
+            }
+            primes.add(i);
+        }
+//        return primes.toArray();
+        return primes.get(primes.size() - 1);
     }
 
 }
--- src/till10/Problem7.java
+++ src/till10/Problem7.java
@@ -1,5 +1,7 @@
 package till10;
 
+import java.util.ArrayList;
+
 public class Problem7 {
     public Problem7() {
         long startTime = System.nanoTime();
@@ -15,7 +17,7 @@ public class Problem7 {
                     continue outer;
                 }
             }
-            System.out.println(i);
+//            System.out.println(i);
             //check if it's the 10001st:
             if (++counter == 10001) {
                 result = i;
@@ -23,6 +25,24 @@ public class Problem7 {
             }
         }
         //shaved off from 1,5sec to /3ms
+
+/*
+        // Faster:
+        ArrayList<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        outer:
+        for (int i = 3; primes.size() < 10001; i += 2) {
+            int sqrt = (int) Math.sqrt(i);
+            for (int j = 0; j < primes.size() && primes.get(j) <= sqrt; j++) {
+                if (i % primes.get(j) == 0) {
+                    continue outer;
+                }
+            }
+            primes.add(i);
+        }
+//        return primes.toArray();
+        result = primes.get(primes.size() - 1);
+*/
 
         long timeToResolve = System.nanoTime() - startTime;
         if (((double) timeToResolve / 1_000_000) >= 1000) {
