6b65a6632471c03913d5d28252583369b9073818
TheSwerik <derswerik@gmail.com>
2019-12-13 11:33:07 +0100

    faster

--- src/till20/Problem15.java
+++ src/till20/Problem15.java
@@ -11,13 +11,13 @@ public class Problem15 {
         // Solution:
         BigInteger n = new BigInteger("1");
         BigInteger k = new BigInteger("1");
-        for (int i = 2; i <= 40; i++) {
+        for (int i = 21; i <= 40; i++) {
             n = n.multiply(new BigInteger(i + ""));
         }
         for (int i = 2; i <= 20; i++) {
             k = k.multiply(new BigInteger(i + ""));
         }
-        n = n.divide(k.multiply(k));
+        n = n.divide(k);
 
         long timeToResolve = System.nanoTime() - startTime;
         System.out.println("Result:\t" + n + "\tTime:\t" + (((double) timeToResolve / 1_000_000) > 1000 ?
