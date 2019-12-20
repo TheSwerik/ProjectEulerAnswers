78ca10db2348069b704f30da9a7a6fc1df1a55f0
TheSwerik <derswerik@gmail.com>
2019-12-20 14:34:16 +0100

    Problem35 optimized

--- src/till40/Problem35.java
+++ src/till40/Problem35.java
@@ -17,10 +17,9 @@ public class Problem35 {
         HashSet<Integer> circularPrimes = new HashSet<>();
         for (Integer prime : primes) {
             if (circularPrimes.contains(prime)) continue;
-
             Integer[] primeSwapped = swapped(prime);
 
-            if (isPrimes(primeSwapped)) {
+            if (primeSwapped != null) {
                 circularPrimes.addAll(Arrays.asList(primeSwapped));
             }
         }
@@ -34,6 +33,12 @@ public class Problem35 {
 
     private Integer[] swapped(int n) {
         String asString = n + "";
+
+        //Optimized:
+        if (n > 10 && (asString.contains("0") || asString.contains("2") || asString.contains("4") || asString.contains("5") || asString.contains("6") || asString.contains("8"))) {
+            return null;
+        }
+
         String[] permutations = new String[asString.length()];
         HashSet<Integer> primeSwapped = new HashSet<>();
         permutations[0] = asString;
@@ -41,18 +46,12 @@ public class Problem35 {
 
         for (int i = 1; i < permutations.length; i++) {
             permutations[i] = (permutations[i - 1].substring(1) + permutations[i - 1].charAt(0));
+            if (!primes.contains(Integer.parseInt(permutations[i]))) {
+                return null;
+            }
             primeSwapped.add(Integer.parseInt(permutations[i]));
         }
         return primeSwapped.toArray(new Integer[0]);
-    }
-
-    private boolean isPrimes(Integer[] arr) {
-        for (Integer i : arr) {
-            if (!primes.contains(i)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public ArrayList<Integer> genPrimes(int range) {
