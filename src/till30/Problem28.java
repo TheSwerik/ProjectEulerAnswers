f720e6e833de8b4b0cf09517987e9b0ad1313bda
TheSwerik <derswerik@gmail.com>
2019-12-20 08:47:18 +0100

    Problem28

--- src/till30/Problem28.java
+++ src/till30/Problem28.java
@@ -1,31 +1,10 @@
 package till30;
 
-import java.util.Arrays;
-
 public class Problem28 {
 
     public Problem28() {
         long startTime = System.nanoTime();
         long result = 1;
-
-        // Grid:
-/*        int n = 1001;
-        long[][] spiral = new long[n][n];
-        long counter = n * n;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                if (i % 4 == 0) { // go right
-                    spiral[j][i] = counter--;
-                } else if (i % 4 == 3) {
-                    spiral[1000 - i][j] = counter--;
-                } else if (i % 4 == 2) {
-                    spiral[1000 - j][1000 - i] = counter--;
-                } else {
-                    spiral[i][1000 - j - i] = counter--;
-                }
-            }
-        }
-        System.out.println(Arrays.deepToString(spiral));*/
 
         //schlau
         int step = 2;
