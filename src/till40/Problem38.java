39bc49ed09ea117ab9b8bbaa3fc0e580cc95bdff
TheSwerik <derswerik@gmail.com>
2020-01-10 23:24:24 +0100

    Problem38

---
+++ src/till40/Problem38.java
@@ -0,0 +1,64 @@
+package till40;
+
+import java.util.Arrays;
+
+public class Problem38 {
+
+    private int counter = 0;
+
+    public Problem38() {
+        long startTime = System.nanoTime();
+        long result = 0;
+
+        // Solution:
+        int n = 362880;
+        String[] permutations = new String[n];
+        String first = "987654321";
+
+        this.permute(permutations, "", first);
+
+
+        loop:
+        for (String number : permutations) {
+            if (Integer.parseInt(number.substring(0, 4)) * 2 > 100_000) {
+                continue;
+            }
+//            System.out.println(number);
+            for (int i = Integer.parseInt(number.substring(0, 4)); i > 0; i--) {
+                if (concatenate(i).equals(number)) {
+                    result = Integer.parseInt(number);
+                    break loop;
+                }
+            }
+        }
+
+        long timeToResolve = System.nanoTime() - startTime;
+        System.out.println("Result:\t" + result + "\tTime:\t" + (((double) timeToResolve / 1_000_000) > 1000 ?
+                (((double) timeToResolve / 1_000_000_000) + "s") :
+                (((double) timeToResolve / 1_000_000) + "ms")));
+    }
+
+    private void permute(String[] permutations, String prefix, String s) {
+        if (counter >= 362880) {
+            return;
+        }
+        int n = s.length();
+        if (n == 0) {
+            permutations[this.counter++] = prefix;
+        } else {
+            for (int i = 0; i < n; i++) {
+                permute(permutations, prefix + s.charAt(i), s.substring(0, i) + s.substring(i + 1, n));
+            }
+        }
+    }
+
+    private String concatenate(int n) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; result.length() < 9; i++) {
+            result.append(n * i);
+        }
+
+        return result.toString();
+    }
+}
