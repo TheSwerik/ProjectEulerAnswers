157a23e8804a47e2b74013ee1cc5bd146830a944
TheSwerik <derswerik@gmail.com>
2020-01-10 22:42:35 +0100

    cleanup

--- src/till40/Problem37.java
+++ src/till40/Problem37.java
@@ -12,37 +12,77 @@ public class Problem37 {
 //        result = solveSlow();
 //        result = solveSlowButOptimized();
 
+        //gen all primes < 1000000:
+        ArrayList<Integer> primes = new ArrayList<Integer>(Arrays.asList(primeSieveButFast(1_000_000)));
+
         int count = 0;
         String[] firstPrimesArr = new String[]{"2", "3", "5", "7"};
         ArrayList<String> firstPrimes = new ArrayList<>(Arrays.asList(firstPrimesArr));
-        ArrayList<Integer> primes = new ArrayList<Integer>(Arrays.asList(primeSieveButFast(1_000_000)));
         for (int i = 0; i < 11; ) {
             ArrayList<String> newPrimes = new ArrayList<>();
 
             for (String prime : firstPrimes) {
                 outer:
                 for (int j = 0; j < 10; j++) {
+                    //generate primes:
                     String s = j + "" + prime;
                     if (primes.contains(Integer.parseInt(s))) {
                         newPrimes.add(s);
                     }
 
+                    //check if valid:
                     for (int k = 1; k < s.length(); k++) {
                         if (!primes.contains(Integer.parseInt(s.substring(k))) ||
                                 !primes.contains(Integer.parseInt(s.substring(0, s.length() - k)))) {
                             continue outer;
                         }
                     }
+
+                    //when valid:
                     i++;
                     result += Integer.parseInt(s);
                 }
             }
+            firstPrimes = newPrimes;
         }
 
         long timeToResolve = System.nanoTime() - startTime;
         System.out.println("Result:\t" + result + "\tTime:\t" + (((double) timeToResolve / 1_000_000) > 1000 ?
                 (((double) timeToResolve / 1_000_000_000) + "s") :
                 (((double) timeToResolve / 1_000_000) + "ms")));
+    }
+
+    private long solveSlowButOptimized() {
+        long result = 0;
+
+        int count = 0;
+        ArrayList<Integer> primes = new ArrayList<Integer>(Arrays.asList(primeSieveButFast(1_000_000)));
+        outer:
+        for (Integer i : primes) {
+            String testString = i + "";
+
+            //skip if <10 or contains 5 (except leading)
+            if (i < 10 ||
+                    testString.substring(1).contains("5")) {
+                continue;
+            }
+
+            //check if valid
+            for (int j = 1; j < testString.length(); j++) {
+                if (!primes.contains(Integer.parseInt(testString.substring(j))) ||
+                        !primes.contains(Integer.parseInt(testString.substring(0, testString.length() - j)))) {
+                    continue outer;
+                }
+            }
+
+            //when valid
+            result += i;
+            if (++count == 11) {
+                break;
+            }
+        }
+
+        return result;
     }
 
     private long solveSlow() {
@@ -71,34 +111,6 @@ public class Problem37 {
                 }
             }
         }
-        return result;
-    }
-
-    private long solveSlowButOptimized() {
-        long result = 0;
-
-        int count = 0;
-        ArrayList<Integer> primes = new ArrayList<Integer>(Arrays.asList(primeSieveButFast(1_000_000)));
-        outer:
-        for (Integer i : primes) {
-            String testString = i + "";
-            if (i < 10 ||
-                    testString.substring(1).contains("5")) {
-                continue;
-            }
-            for (int j = 1; j < testString.length(); j++) {
-                if (!primes.contains(Integer.parseInt(testString.substring(j))) ||
-                        !primes.contains(Integer.parseInt(testString.substring(0, testString.length() - j)))) {
-                    continue outer;
-                }
-            }
-            System.out.println(i);
-            result += i;
-            if (++count == 11) {
-                break;
-            }
-        }
-
         return result;
     }
 
