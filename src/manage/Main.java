8d3354d45c9b45c456cfa521eb0d6e9369eabf3c
TheSwerik <derswerik@gmail.com>
2020-01-10 21:35:39 +0100

    Test

--- src/manage/Main.java
+++ src/manage/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //a
         while (true) {
             System.out.println("\nWhich do you want to try out?");
             int input = sc.nextInt();
