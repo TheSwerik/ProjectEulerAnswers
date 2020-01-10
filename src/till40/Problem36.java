47ce4d78bd60f9a89aadf59d0e1a6e997fcac1e9
TheSwerik <derswerik@gmail.com>
2020-01-10 08:21:04 +0100

    Problem36

--- src/till40/Problem36.java
+++ src/till40/Problem36.java
@@ -1,7 +1,5 @@
 package till40;
 
-import java.util.ArrayList;
-
 public class Problem36 {
 
     public Problem36() {
@@ -12,11 +10,11 @@ public class Problem36 {
         int counter = 0;
         String nines = "99";
         String tens = "0";
+        // under 10,000:
         for (long i = 11; i < 10_000; ) {
             String binary = Integer.toBinaryString((int) i);
             if (binary.equals(new StringBuilder(binary).reverse().toString())) {
                 result += i;
-                System.out.println(i);
             }
             // 99 or 999 or 9999 or 99999
             if (i % Integer.parseInt(nines) == 0) {
@@ -43,75 +41,37 @@ public class Problem36 {
                 i += 11;
             }
         }
-/*        // for 10101:
-        int compare = 19191;
-        int lastI = 100001;
-        for (long i = 10101; i < 100_000; ) {
-            if (!checkList.contains((int) i)) {
-                if (Integer.toBinaryString((int) i).equals(new StringBuilder(Integer.toBinaryString((int) i)).reverse().toString())) {
-                    result += i;
-                    System.out.println(i);
-                }
-            }
-            //TODO find 11x11 & 22x22 & 15x51 etc
-
-            // 19191 and 29292 etc
-            if (i % compare == 0) {
-                compare += 10101;
-                i += 1011;
-                counter = 0;
-                continue;
-            }
-
-            if (++counter == 10) {
-                i += 11;
-                counter = 0;
-            } else {
-                i += 1010;
-            }
-            System.out.println(i + " " + counter);
-        }*/
-        // for 10101 and 11011:
+        // over 10,000:
         counter = 0;
         long lastI = 10001;
-        for (long i = lastI, j = 0; i < 100_000; ) {
-            if (Integer.toBinaryString((int) i).equals(new StringBuilder(Integer.toBinaryString((int) i)).reverse().toString())) {
-                result += i;
-//                System.out.println(i);
-            }
-            i += 1010;
-            if (++counter == 10) {
-                i = lastI + 100;
-                lastI = (int) i;
-                counter = 0;
-                j++;
-            } // 19991 and 29992 etc
-            if (j == 10) {
-                j = 0;
-                i += 9001;
-                lastI = (int) i;
-            }
-            System.out.println(i);
-        }
-        // for 101101 and 110011:
-        counter = 0;
-        lastI = 100001;
+        int addNumber1 = 1010;
+        int addNumber2 = 100;
+        int addNumber3 = 9001;
         for (long i = lastI, j = 0; i < 1_000_000; ) {
             if (Integer.toBinaryString((int) i).equals(new StringBuilder(Integer.toBinaryString((int) i)).reverse().toString())) {
                 result += i;
-//                System.out.println(i);
             }
-            i += 10010;
+            i += addNumber1;
+            // when middle number(s) are 9 (xx9xx or xx99xx):
             if (++counter == 10) {
-                i = lastI + 1100;
+                i = lastI + addNumber2;
                 lastI = (int) i;
                 counter = 0;
                 j++;
-            } // 199991 and 299992 etc
+            }
+            // when all inner numbers are 9 (x999x or x9999x):
             if (j == 10) {
                 j = 0;
-                i += 89001;
+                i += addNumber3;
                 lastI = (int) i;
+            }
+            // change numbers when over 100,000:
+            if (i == 100010) {
+                lastI = i = 100001;
+                counter = 0;
+                addNumber1 = 10010;
+                addNumber2 = 1100;
+                addNumber3 = 89001;
             }
         }
 
