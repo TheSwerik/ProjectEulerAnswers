eccc2a2afefb490e8c050a3a644ba2a08a3ba958
TheSwerik <derswerik@gmail.com>
2020-01-16 11:45:42 +0100

    IM SOO CLOSE

--- src/till690/Problem684.java
+++ src/till690/Problem684.java
@@ -8,22 +8,27 @@ public class Problem684 {
 
     public Problem684() {
         long startTime = System.nanoTime();
-        BigInteger result = BigInteger.ZERO;
+        long result = 0;
 
         // Solution:
-        System.out.println("999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999".length());
         long[] fibo = findFibos(90);
-        for (int i = 10000; i < 100000; ) {
-            result = result.add(findSmallestDigitSum(i));
-            if (i++ % 9999 == 0) {
-                System.out.println(result.toString());
-                bigS(i);
-                result = BigInteger.ZERO;
-            }
+//        for (int i = 1000; i < 10000; ) {
+//            result = result.add(findSmallestDigitSum(i));
+//            if (i++ % 999 == 0) {
+//                System.out.println(result.toString());
+//                bigS(i);
+//                result = BigInteger.ZERO;
+//            }
+//        }
+        float i = 1;
+        for (long f : fibo) {
+            result += bigS(f) % 1_000_000_007;
+            System.out.println(i++ / 90 * 100 + " %");
+//            i++;
         }
 
         long timeToResolve = System.nanoTime() - startTime;
-        System.out.println("Result:\t" + result.toString() + "\tTime:\t" + (((double) timeToResolve / 1_000_000) > 1000 ?
+        System.out.println("Result:\t" + result + "\tTime:\t" + (((double) timeToResolve / 1_000_000) > 1000 ?
                 (((double) timeToResolve / 1_000_000_000) + "s") :
                 (((double) timeToResolve / 1_000_000) + "ms")));
     }
@@ -53,48 +58,43 @@ public class Problem684 {
         if (temp.length() > 1) {
             firstNumberOfNines = Integer.parseInt("1".repeat((n + "").length() - 2) + "0");
         }
+
         int nextNumberOfNines = 0;
         if (temp.length() > 2) {
-            for (int i = 0; i < temp.length() - 2; i++) {
+            for (int i = 1; i <= temp.length() - 2; i++) {
                 nextNumberOfNines += Integer.parseInt("9".repeat(i));
             }
         }
-        int lastNumberOfNines = temp.length() - 1;
+
+
         while (n > 0) {
             String test = n + "";
             switch (test.length()) {
                 case 1:
                     result = result.add(BigInteger.valueOf(oneToNine[(int) n]));
                     n -= n;
-                case 2:
-                    result = result.add(new BigInteger("5" + "9".repeat(0) + "3" + "".repeat(Integer.parseInt(test.substring(0, 1))) + "9".repeat(Integer.parseInt(test.substring(0, 1)) - 1) + "1")); //1
-                    n -= (test.charAt(0) - 48) * 10;
-                case 3:
-                    result = result.add(new BigInteger("5" + "9".repeat(10) + "3" + "999999999".repeat(Integer.parseInt(test.substring(0, 1))) + "99".repeat(Integer.parseInt(test.substring(0, 1)) - 1) + "01")); // 9
-                    n -= (test.charAt(0) - 48) * 100;
-                case 4:
-                    result = result.add(new BigInteger("5" + "9".repeat(110) + "3" + "999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999".repeat(Integer.parseInt(test.substring(0, 1))) + "999".repeat(Integer.parseInt(test.substring(0, 1)) - 1) + "001")); // 9+99
-                    n -= (test.charAt(0) - 48) * 1000;
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                case 12:
-                case 13:
-                case 14:
-                case 15:
-                case 16:
-                case 17:
-                case 18:
+                    break;
+//                case 2:
+//                    result = result.add(new BigInteger("5" + "9".repeat(0) + "3" + "".repeat(Integer.parseInt(test.substring(0, 1))) + "9".repeat(Integer.parseInt(test.substring(0, 1)) - 1) + "1")); //1
+//                    n -= (test.charAt(0) - 48) * 10;
+//                case 3:
+//                    result = result.add(new BigInteger("5" + "9".repeat(10) + "3" + "999999999".repeat(Integer.parseInt(test.substring(0, 1))) + "99".repeat(Integer.parseInt(test.substring(0, 1)) - 1) + "01")); // 9
+//                    n -= (test.charAt(0) - 48) * 100;
+//                case 4:
+//                    result = result.add(new BigInteger("5" + "9".repeat(110) + "3" + "999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999".repeat(Integer.parseInt(test.substring(0, 1))) + "999".repeat(Integer.parseInt(test.substring(0, 1)) - 1) + "001")); // 9+99
+//                    n -= (test.charAt(0) - 48) * 1000;
                 default:
-                    result = result.add(new BigInteger("5" + "9".repeat(firstNumberOfNines) + "3" + ("9".repeat(nextNumberOfNines)).repeat(Integer.parseInt(test.substring(0, 1))) + ("9".repeat(lastNumberOfNines++)).repeat(Integer.parseInt(test.substring(0, 1)) - 1) + "0".repeat(lastNumberOfNines - 2) + "1"));
-                    firstNumberOfNines = Integer.parseInt((firstNumberOfNines + "").substring(1));
-                    nextNumberOfNines -= Integer.parseInt("9".repeat(test.length() - 2));
+                    result = result.add(new BigInteger("5" + "9".repeat(firstNumberOfNines) + "3" +
+                            ("9".repeat(nextNumberOfNines)).repeat(Integer.parseInt(test.substring(0, 1))) +
+                            ("9".repeat(test.length() - 1)).repeat(Integer.parseInt(test.substring(0, 1)) - 1) +
+                            "0".repeat(test.length() - 2) + "1"));
+                    if (firstNumberOfNines > 0)
+                        firstNumberOfNines = Integer.parseInt((firstNumberOfNines + "").substring(1));
+                    if (test.length() > 2)
+                        nextNumberOfNines -= Integer.parseInt("9".repeat(test.length() - 2));
 
-                    n -= (test.charAt(0) - 48) * 1000;
+                    n -= (test.charAt(0) - 48) * Integer.parseInt("1" + "0".repeat(test.length() - 1));
+//                    System.out.println(result);
             }
         }
         return Long.parseLong(result.mod(new BigInteger("1000000007")).toString());
