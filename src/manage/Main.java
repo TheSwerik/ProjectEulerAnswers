2266689da764c36378710e51fa32cef8cfedf515
TheSwerik <derswerik@gmail.com>
2020-01-17 15:27:40 +0100

    add C++ Support

--- src/manage/Main.java
+++ src/manage/Main.java
@@ -1,5 +1,9 @@
 package manage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
@@ -36,10 +40,35 @@ public class Main {
     }
 
     private static void startCpp(String inputString) {
+        int inputInt = Integer.parseInt(inputString);
+        compileAndRunC("cpp\\problems" + "\\Problem" + "0".repeat(4 - inputString.length()) + inputString);
+    }
+
+    private static void compileAndRunC(String path) {
         try {
-            int inputInt = Integer.parseInt(inputString);
-            Class.forName("cpp.problems" + ".Problem" + "0".repeat(4 - inputString.length()) + inputString).getDeclaredConstructor().newInstance();
-        } catch (ClassNotFoundException | InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            //get Path & Command:
+            String pathIn = new File("").getAbsolutePath() + "\\src\\" + path + ".c";
+            String pathOut = new File("").getAbsolutePath() + "\\out\\exe" + path.substring(path.lastIndexOf('\\')) + ".exe";
+            String pathToCmd = "C:\\Windows\\System32";
+            String command = "g++ " + pathIn + " -o " + pathOut;
+
+            //Start Compile Process
+            Process p = new ProcessBuilder("cmd", "/c", command).directory(new File(pathToCmd)).start();
+
+            //Konsolen Output:
+            String s = null;
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            //Start built Process
+            p = new ProcessBuilder(pathOut).start();
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((s = input.readLine()) != null) {
+                System.out.println(s + "\n");
+            }
+        } catch (IOException e) {
             System.out.println("not valid\n");
             e.printStackTrace();
         }
