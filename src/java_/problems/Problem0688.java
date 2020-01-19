442e552f227dce0ea11ff99ac4510ba3078d6d0e
TheSwerik <derswerik@gmail.com>
2020-01-19 15:25:21 +0100

    Problem 688 should work, still takes hours

--- src/java_/problems/Problem0688.java
+++ src/java_/problems/Problem0688.java
@@ -12,16 +12,18 @@ public class Problem0688 {
 
         // Solution:
         long max = 10_000_000_000_000_000L;
-        long gausMax = 4_294_967_295L;
-        long tempMax = 100_000;
-        System.out.println(gaus(gausMax + 1));
-        System.exit(0);
-//        long tempMax = max;
+//        long gausMax = 4_294_967_295L; // max for long
+        long gausMax = findGausMax(10_000_000_000_000_000L); // max for max
+        long tempMax = 1_000_000;
         int percent = 0;
-        for (long n = tempMax; n <= tempMax; n++) {
-
+        outer:
+        for (long n = 1; n <= tempMax; n++) {
+            // F(n):
             for (long k = 1; k <= n; k++) {
-                if (k > gausMax && n > (k * k + k) / 2) {
+                if (k > gausMax) {
+                    break outer;
+                }
+                if (n < (k * k + k) / 2) {
                     continue;
                 }
                 if (k % 2 == 0) {
@@ -38,6 +40,7 @@ public class Problem0688 {
                 System.out.println((percent = temp) + "%");
             }
         }
+        result += Long.parseLong((new BigInteger(F(gausMax) + "").multiply(new BigInteger((max - gausMax - 1)+ "")).mod(mod)).toString());
 
 
         long timeToResolve = System.nanoTime() - startTime;
@@ -68,7 +71,28 @@ public class Problem0688 {
     private long gaus(long k) {
         BigInteger kB = new BigInteger(k + "");
         BigInteger result = new BigInteger(k + "");
-        result = ((kB.multiply(kB)).add(kB)).divide(BigInteger.TWO).mod(mod);
+        result = ((kB.multiply(kB)).add(kB)).divide(BigInteger.TWO);
+        return Long.parseLong(result.toString());
+    }
+
+    private long findGausMax(long max) {
+        StringBuilder result = new StringBuilder();
+        String maxS = max + "";
+        outer:
+        for (int i = maxS.length() - 2; i >= 0; i--) {
+            for (int j = 10; j >= 0; j--) {
+                long temp = Long.parseLong(result.toString() + j + "0".repeat(i));
+
+                if (temp > 4_294_967_295L) { // too big for Long
+                    continue;
+                }
+
+                if (max - gaus(temp) > 0) {
+                    result.append(j);
+                    continue outer;
+                }
+            }
+        }
         return Long.parseLong(result.toString());
     }
 }
