d3fe6fb7d409a5ad99a75ce1c6644d75dd91a31d
TheSwerik <derswerik@gmail.com>
2020-01-20 21:27:17 +0100

    add c++ support

---
+++ src/manage/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../cpp/problems/Problem0001.c"
+
+int main() {
+	std::cout << "Hello, World!\n";
+	std::cout << fibonacci(10) << std::endl;
+
+	//system("pause");
+	return 0;
+}
