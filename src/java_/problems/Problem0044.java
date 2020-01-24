ba8ec3acc218fe6bbb4fcfb743c91e23c44d3b9c
TheSwerik <derswerik@gmail.com>
2020-01-24 13:26:52 +0100

    Problem 44 revert to fast

--- src/java_/problems/Problem0044.java
+++ src/java_/problems/Problem0044.java
@@ -1,25 +1,24 @@
 package java_.problems;
 
-import de.swerik.fraction.Fraction;
+import java.util.Arrays;
 
 public class Problem0044 {
-
-    private Fraction p = new Fraction(1, 6);
 
     public Problem0044() {
         long startTime = System.nanoTime();
         long result = 0;
 
         // Solution:
+        long[] arr = fillarr(2500);
         outer:
         for (int i = 1; ; i++) {
-            long l1 = i * (3 * i - 1) / 2L;
-            for (int j = 1; j < i; j++) {
-                long l2 = j * (3 * j - 1) / 2L;
-                if (isPartOfFormular(l1 + l2) && isPartOfFormular(l1 - l2)) {
+            long l1 = arr[i];
+            for (int j = 1; j <= i; j++) {
+                long l2 = arr[j];
+                if (isPartOfFormular(arr,l1 + l2) && isPartOfFormular(arr,l1 - l2)) {
                     result = l1 - l2 > 0 ? l1 - l2 : l2 - l1;
-//                    System.out.println(l1 + " at " + i);
-//                    System.out.println(l2 + " at " + j);
+                    System.out.println(l1 + " at " + i);
+                    System.out.println(l2 + " at " + j);
                     break outer;
                 }
             }
@@ -31,10 +30,17 @@ public class Problem0044 {
                 (((double) timeToResolve / 1_000_000) + "ms")));
     }
 
-    private boolean isPartOfFormular(long n) {
-        if (n < 0) {
-            n = -n;
+    private long[] fillarr(int n) {
+        long[] arr = new long[n];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i - 1] = i * (3 * i - 1) / 2L;
         }
-        return p.add(p.pow2().add(new Fraction(2 * n, 3)).sqrt()).getDenominator() == 1;
+
+        return arr;
+    }
+
+    private boolean isPartOfFormular(long[] pentagonal, long value) {
+        return Arrays.binarySearch(pentagonal, value) >= 0;
     }
 }
