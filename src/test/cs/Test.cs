517604d2523518b579e561cd7a7c3ec39cfe5280
TheSwerik <derswerik@gmail.com>
2020-03-02 13:45:57 +0100

    add c#

---
+++ src/test/cs/Test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Euler.test.cs
+{
+    public class sdaasd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
