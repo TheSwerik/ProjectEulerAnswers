25aca2945968d9c70877f2677abb9fb139cf8a24
TheSwerik <derswerik@gmail.com>
2020-03-02 17:04:20 +0100

    compare 206 to c#

---
+++ src/main/cs/Problem0206.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+public class Problem0206
+{
+    public Problem0206()
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        UInt64 result = 0;
+
+        // Solution:
+        UInt64 start = (UInt64) Math.Sqrt(1020304050607080900.0);
+        UInt64 end = (UInt64) Math.Sqrt(1929394959697989990.0);
+        // String pattern = "1.2.3.4.5.6.7.8.9.0";
+        // Regex regex = new Regex(pattern);
+        
+        for (UInt64 i = start; i <= end; i += 10)
+        {
+            // if (regex.IsMatch((i * i) + ""))
+            // {
+            //     result = i;
+            //     break;
+            // }
+
+            String s = "" + (i * i);
+            if (s.Length != 19)
+            {
+                continue;
+            }
+
+            if (s[16] == '9' &&
+                s[14] == '8' &&
+                s[12] == '7' &&
+                s[10] == '6' &&
+                s[8] == '5' &&
+                s[6] == '4' &&
+                s[4] == '3' &&
+                s[2] == '2' &&
+                s[0] == '1' &&
+                s[18] == '0')
+            {
+                result = i;
+                break;
+            }
+        }
+
+        stopWatch.Stop();
+        string test = stopWatch.Elapsed.ToString();
+        Console.WriteLine("Result:\t" + result + "\tTime:\t" +
+                          (double.Parse(test.Substring(test.LastIndexOf(":") + 1, 2)) >= 1
+                              ? double.Parse(test.Substring(test.LastIndexOf(":") + 1)) + " s"
+                              : double.Parse(test.Substring(test.IndexOf(".") + 1)) / 10_000 + " ms"));
+    }
+}
