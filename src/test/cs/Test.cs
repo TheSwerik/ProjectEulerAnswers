e4c2d98a8acd3a87bd3c1480b400d599df4b69ce
TheSwerik <derswerik@gmail.com>
2020-03-05 11:25:26 +0100

    c# new Main

--- src/test/cs/Test.cs
+++ src/test/cs/Test.cs
@@ -10,17 +10,10 @@ namespace Euler.test.cs
         static void Main(string[] args)
         {
             Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
-            while (true)
+            if (args.Length > 0)
             {
-                Console.WriteLine("\nWhich do you want to try out?");
-                string input = Console.ReadLine();
-                if (input.Equals("0"))
-                {
-                    return;
-                }
-
-                //start Problems:
-                string path =  "Problem" + new string('0', 4 - input.Length) + input;
+                //start Problem:
+                string path = "Problem" + args[0];
                 Type problem = Type.GetType(path);
                 if (problem == null)
                 {
@@ -31,6 +24,32 @@ namespace Euler.test.cs
                 else
                 {
                     Activator.CreateInstance(problem);
+                }
+            }
+            else
+            {
+                while (true)
+                {
+                    Console.WriteLine("\nWhich do you want to try out?");
+                    string input = Console.ReadLine();
+                    if (input.Equals("0"))
+                    {
+                        return;
+                    }
+
+                    //start Problems:
+                    string path = "Problem" + new string('0', 4 - input.Length) + input;
+                    Type problem = Type.GetType(path);
+                    if (problem == null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Cannot find the Class: " + path);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Activator.CreateInstance(problem);
+                    }
                 }
             }
         }
