2f3d125047153fbf98c19a3f39d902411bca0700
TheSwerik <derswerik@gmail.com>
2020-03-06 23:24:59 +0100

    add Template

---
+++ src/main/cs/ProblemTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Euler.test.cs;
+
+namespace Euler.main.cs
+{
+    public class ProblemTemplate
+    {
+        public ProblemTemplate()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            ulong result = 0;
+
+            // Solution:
+
+
+
+            stopWatch.Stop();
+            string elapsedTime = stopWatch.Elapsed.ToString();
+            Console.WriteLine("Result:\t" + result + "\tTime:\t" +
+                              (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
+                                  ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
+                                  : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
+                                    " ms"));
+            if (Test.DoBenchmark)
+            {
+                Benchmark.AddTime(0, double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000);
+            }
+        }
+    }
+}
