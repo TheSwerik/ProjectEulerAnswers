4b968a09a34ab33f84e8693f0b6a87e4376bb06a
TheSwerik <derswerik@gmail.com>
2020-03-06 21:46:25 +0100

    C# Test fixed

--- src/test/cs/Test.cs
+++ src/test/cs/Test.cs
@@ -7,7 +7,7 @@ namespace Euler.test.cs
 {
     public class Test
     {
-        public const bool DoBenchmark = true;
+        public const bool DoBenchmark = false;
 
         static void Main(string[] args)
         {
@@ -17,10 +17,18 @@ namespace Euler.test.cs
                 Benchmark benchmark = new Benchmark();
                 return;
             }
-            if (args.Length > 0)
+
+            while (true)
             {
-                //start Problem:
-                string path = "Problem" + args[0];
+                Console.WriteLine("\nWhich do you want to try out?");
+                string input = Console.ReadLine();
+                if (input.Equals("0"))
+                {
+                    return;
+                }
+
+                //start Problems:
+                string path = "Euler.main.cs.Problem" + new string('0', 4 - input.Length) + input;
                 Type problem = Type.GetType(path);
                 if (problem == null)
                 {
@@ -31,32 +39,6 @@ namespace Euler.test.cs
                 else
                 {
                     Activator.CreateInstance(problem);
-                }
-            }
-            else
-            {
-                while (true)
-                {
-                    Console.WriteLine("\nWhich do you want to try out?");
-                    string input = Console.ReadLine();
-                    if (input.Equals("0"))
-                    {
-                        return;
-                    }
-
-                    //start Problems:
-                    string path = "Problem" + new string('0', 4 - input.Length) + input;
-                    Type problem = Type.GetType(path);
-                    if (problem == null)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Cannot find the Class: " + path);
-                        Console.ResetColor();
-                    }
-                    else
-                    {
-                        Activator.CreateInstance(problem);
-                    }
                 }
             }
         }
