afdc7d46120b195771e638903e499aabcefde969
TheSwerik <derswerik@gmail.com>
2020-03-07 22:37:45 +0100

    add Java 46-50

---
+++ src/main/java/Problem0047.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Problem0047 {
+    private Integer[] primes;
+
+    public Problem0047() {
+        long startTime = System.nanoTime();
+        long result = 0;
+
+        // Solution:
+        primes = primeSieveButFast(150_000);
+        for (int i = 20; ; i++) {
+            if (Arrays.binarySearch(primes, i) >= 0) continue;
+            if (numberOfPrimFactors(new long[]{i, i + 1, i + 2, i + 3})) {
+                result = i;
+                break;
+            }
+        }
+
+        long timeToResolve = System.nanoTime() - startTime;
+        System.out.println("Result:\t" + result + "\tTime:\t" + (((double) timeToResolve / 1_000_000) > 1000 ?
+                (((double) timeToResolve / 1_000_000_000) + "s") :
+                (((double) timeToResolve / 1_000_000) + "ms")));
+    }
+
+    private boolean numberOfPrimFactors(long[] numbers) {
+        for (var j = 0; j < numbers.length; j++) {
+            var count = 0;
+            var n = numbers[j];
+            for (int i = 0; primes[i] <= n; i++) {
+                if (n % primes[i] == 0) count++;
+
+                while (n % primes[i] == 0) n /= primes[i];
+            }
+
+            if (count != 4) return false;
+        }
+
+        return true;
+    }
+
+    public static Integer[] primeSieveButFast(int range) {
+        boolean[] bools = new boolean[range + 1];
+        Arrays.fill(bools, true);
+        double root = Math.sqrt(range) + 0.5;
+        for (int i = 3; i < root; i += 2) {
+            if (bools[i]) {
+                for (int j = i * i; j < range; j += i * 2) {
+                    bools[j] = false;
+                }
+            }
+        }
+        ArrayList<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        for (int i = 3; i < range; i += 2) {
+            if (bools[i]) {
+                primes.add(i);
+            }
+        }
+        return primes.toArray(new Integer[0]);
+    }
+}
