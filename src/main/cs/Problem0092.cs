26f2c200ff4a0f3d878fa1599367847322f3d875
TheSwerik <derswerik@gmail.com>
2020-03-08 19:08:52 +0100

    92 slow

---
+++ src/main/cs/Problem0092.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using Euler.test.cs;
+
+namespace Euler.main.cs
+{
+    public class Problem0092
+    {
+        public Problem0092()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            ulong result = 0;
+
+            // Solution:
+            for (int i = 2; i < 10_000_000; i++)
+            {
+                string number = i + "";
+                while (number != "1")
+                {
+                    long  temp = 0;
+                    foreach (char c in number)
+                    {
+                        temp += ((c - 48) * (c - 48));
+                    }
+
+                    number = temp + "";
+                    if (number.Equals("89"))
+                    {
+                        result++;
+                        break;
+                    }
+                }
+            }
+
+
+            stopWatch.Stop();
+            string elapsedTime = stopWatch.Elapsed.ToString();
+            Console.WriteLine("Result:\t" + result + "\tTime:\t" +
+                              (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
+                                  ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
+                                  : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
+                                    " ms"));
+            if (Test.DoBenchmark)
+            {
+                Benchmark.AddTime(92, double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000);
+            }
+        }
+    }
+}
