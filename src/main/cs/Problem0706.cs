24da94306846079be190e22d89b7899245f1d7bc
TheSwerik <derswerik@gmail.com>
2020-03-16 17:26:52 +0100

    didnt finish

---
+++ src/main/cs/Problem0706.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using Euler.test.cs;
+
+namespace Euler.main.cs
+{
+    public class Problem0706
+    {
+        public Problem0706()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            BigInteger result = 0;
+
+            // Solution:
+            result = BigF(100_001) % 1_000_000_007;
+
+            System.Console.WriteLine(
+                (((3 * 1150500 / 2267)
+                  + BigInteger.Parse("9" + new string('0', 99999)))
+                    * 2267 / 6750) % 1_000_000_007
+                );
+
+
+            stopWatch.Stop();
+            string elapsedTime = stopWatch.Elapsed.ToString();
+            Console.WriteLine("Result:\t" + result + "\tTime:\t" +
+                              (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
+                                  ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
+                                  : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
+                                    " ms"));
+            if (Test.DoBenchmark)
+            {
+                Benchmark.AddTime(706, double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000);
+            }
+        }
+
+        private BigInteger f(string n)
+        {
+            BigInteger counter = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                for (int j = 1; j <= n.Length - i; j++)
+                {
+                    string sub = n.Substring(i, j);
+                    BigInteger sum = 0;
+                    foreach (char c in sub)
+                    {
+                        sum += c - 48;
+                    }
+
+                    if (sum % 3 == 0) counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        private BigInteger BigF(int digits)
+        {
+            if ((digits + 1) % 3 == 0) return BigInteger.Parse('3' + new String('0', digits - 1));
+            BigInteger result = 0;
+
+            // Solution:
+            BigInteger min = BigInteger.Parse("1" + new String('0', digits - 1));
+            BigInteger max = BigInteger.Parse("1" + new String('0', digits));
+            System.Console.WriteLine("Generated min & max.");
+            for (BigInteger i = min; i < max; i++)
+            {
+                if (f(i + "") % 3 == 0)
+                {
+                    result++;
+                    // System.Console.WriteLine(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
