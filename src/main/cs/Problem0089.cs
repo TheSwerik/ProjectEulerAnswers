f741504c61abfbe2f33076f2459b1e8ee09a0463
TheSwerik <derswerik@gmail.com>
2020-03-19 11:48:58 +0100

    doesnt work like that

--- src/main/cs/Problem0089.cs
+++ src/main/cs/Problem0089.cs
@@ -18,6 +18,51 @@ namespace Euler.main.cs
             long result = 0;
 
             // Solution:
+            Dictionary<char, long> roman = CreateRomanDict();
+            long[] values = CalcValues(lines, roman);
+            string[] cleanRoman = ConvertToRoman(values, roman);
+
+            //calcMax:
+            long max = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                max += lines[i].Length;
+                result += cleanRoman[i].Length;
+            }
+
+
+            stopWatch.Stop();
+            var elapsedTime = stopWatch.Elapsed.ToString();
+            Console.WriteLine("Result:\t" + (result - max) + "\tTime:\t" +
+                              (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
+                                  ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
+                                  : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
+                                    " ms"));
+            if (Test.DoBenchmark)
+                Benchmark.AddTime(89, double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000);
+        }
+
+        private string[] ConvertToRoman(long[] values, Dictionary<char, long> roman)
+        {
+            string[] result = new string[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                for (int j = 0; j < roman.Count; j++)
+                {
+                    while (values[i] > roman.ElementAt(j).Value)
+                    {
+                        values[i] -= roman.ElementAt(j).Value;
+                        result[i] += roman.ElementAt(j).Key;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private Dictionary<char, long> CreateRomanDict()
+        {
             Dictionary<char, long> roman = new Dictionary<char, long>();
             roman.Add('M', 1000);
             roman.Add('D', 500);
@@ -26,10 +71,13 @@ namespace Euler.main.cs
             roman.Add('X', 10);
             roman.Add('V', 5);
             roman.Add('I', 1);
-            // calc values
+            return roman;
+        }
+
+        private long[] CalcValues(string[] lines, Dictionary<char, long> roman)
+        {
             long[] values = new long[lines.Length];
-            // for (int i = 0; i < lines.Length; i++)
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < lines.Length; i++)
             {
                 string line = lines[i];
                 for (int j = 0; j < roman.Count; j++)
@@ -50,28 +98,9 @@ namespace Euler.main.cs
                         line = line.Substring(part.Length);
                     }
                 }
-
-                System.Console.WriteLine(values[0]);
             }
 
-
-            //calcMax:
-            long max = 0;
-            for (int i = 0; i < lines.Length; i++)
-            {
-                max += lines[i].Length;
-            }
-
-
-            stopWatch.Stop();
-            var elapsedTime = stopWatch.Elapsed.ToString();
-            Console.WriteLine("Result:\t" + (result - max) + "\tTime:\t" +
-                              (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
-                                  ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
-                                  : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
-                                    " ms"));
-            if (Test.DoBenchmark)
-                Benchmark.AddTime(89, double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000);
+            return values;
         }
     }
 }
