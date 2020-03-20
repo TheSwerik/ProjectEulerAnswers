7f7aca7552c95b91bc4d80db3df9539efea3ae71
TheSwerik <derswerik@gmail.com>
2020-03-20 15:32:33 +0100

    dkjdhsufsdhliazduzjzafdljdadcijcUCake

--- src/main/cs/Problem0060.cs
+++ src/main/cs/Problem0060.cs
@@ -14,37 +14,62 @@ namespace Euler.main.cs
         {
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            long result = 0;
+            List<long> result = new List<long>();
 
             // Solution:
             List<int> primes = Primes(10000);
             Dictionary<long, List<long>> pairs = new Dictionary<long, List<long>>();
             for (var a = 0; a < primes.Count() - 1; a++)
             {
-                for (var b = a + 1; b < primes.Count(); b++)
+                for (var b = 0; b < primes.Count(); b++)
                 {
-                    if (IsPrime(Convert.ToInt64(primes[a] + "" + primes[b])) && IsPrime(Convert.ToInt64(primes[b] + "" + primes[a])))
+                    if (IsPrime(Convert.ToInt64(primes[a] + "" + primes[b])) &&
+                        IsPrime(Convert.ToInt64(primes[b] + "" + primes[a])))
                     {
-                        if (pairs.ContainsKey(primes[a]))
-                        {
-                            var list = pairs[primes[a]];
-                            list.Add(primes[b]);
-                            pairs[primes[a]] = list;
-                        }
-                        else
-                        {
-                            pairs[primes[a]] = new List<long> {primes[b]};
-                        }
+                        if (pairs.ContainsKey(primes[a])) pairs[primes[a]].Add(primes[b]);
+                        else pairs[primes[a]] = new List<long> {primes[b]};
                     }
                 }
             }
+
             System.Console.WriteLine("generated pairs!");
+            for (int i = 0; i < primes.Count; i++)
+            {
+                if (pairs.ContainsKey(primes[i]) && pairs[primes[i]].Count >= 4)
+                {
+                    int sum = primes[i];
+                    for (int j = 0; j < pairs[primes[i]].Count; j++)
+                    {
+                        if (!pairs.ContainsKey(primes[j])) continue;
+                        sum += primes[j];
+                        for (int k = 0; k < pairs[primes[i]].Count; k++)
+                        {
+                            if (j == k) continue;
+                            if (pairs.ContainsKey(pairs[primes[i]][j]) &&
+                                (pairs[primes[i]].Contains(k) && pairs[pairs[primes[i]][j]].Contains(k)))
+                            {
+                                sum += primes[k];
+                            }
+                        }
+                    }
+                    result.Add(sum);
+                }
+
+                further:
+                {
+                }
+            }
+
 
             end:
             stopWatch.Stop();
             var elapsedTime = stopWatch.Elapsed.ToString();
             System.Console.WriteLine(elapsedTime);
-            Console.WriteLine("Result:\t" + result + "\tTime:\t" +
+            foreach (var a in result)
+            {
+                System.Console.WriteLine(a);
+            }
+            Console.WriteLine("Result:\t" + result.ToString() + "\tTime:\t" +
                               (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
                                   ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
                                   : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
