eec899eb6b74b384ca815b03d2490d4d53232d68
TheSwerik <derswerik@gmail.com>
2020-03-20 16:46:02 +0100

    Ach Junge ey Ich hab kein bock mehr, ganz ehrlich, die Letzten 15 versuchten Probleme sind ungelöst, egal wie einfach oder schwer, mein kopf tut weh, was soll das

---
+++ src/main/cs/Problem0071.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Diagnostics;
+using Euler.test.cs;
+
+namespace Euler.main.cs
+{
+    public class Problem0071
+    {
+        public Problem0071()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            long result = 0;
+
+            // Solution:
+            int goalN = 3;
+            int goalD = 7;
+            int foundD = 1;
+            for (int d = 2; d <= 1_000_000; d++)
+            {
+                for (int n = 1; n < 1_000_000; n++)
+                {
+                    if (d % n != 0 && (double) n / d < (double) goalN / goalD &&
+                        (double) n / d > (double) result / foundD)
+                    {
+                        result = n;
+                        foundD = d;
+                    }
+                }
+            }
+
+            stopWatch.Stop();
+            var elapsedTime = stopWatch.Elapsed.ToString();
+            Console.WriteLine("Result:\t" + result + "\tTime:\t" +
+                              (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
+                                  ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
+                                  : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
+                                    " ms"));
+            if (Test.DoBenchmark)
+                Benchmark.AddTime(71, double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000);
+        }
+    }
+}
---
+++ src/main/cs/Problem0357.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics;
+using Euler.test.cs;
+
+namespace Euler.main.cs
+{
+    public class Problem0357
+    {
+        public Problem0357()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            ulong result = 0;
+
+            // Solution:
+
+            stopWatch.Stop();
+            var elapsedTime = stopWatch.Elapsed.ToString();
+            Console.WriteLine("Result:\t" + result + "\tTime:\t" +
+                              (double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1, 2)) >= 1
+                                  ? double.Parse(elapsedTime.Substring(elapsedTime.LastIndexOf(":") + 1)) + " s"
+                                  : double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000 +
+                                    " ms"));
+            if (Test.DoBenchmark)
+                Benchmark.AddTime(357, double.Parse(elapsedTime.Substring(elapsedTime.IndexOf(".") + 1)) / 10_000);
+        }
+    }
+}
