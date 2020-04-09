fc061a8cab1cd71194384ab80cdd1ec188e71bee
TheSwerik <derswerik@gmail.com>
2020-04-09 10:48:45 +0200

    add empty PyFiles

---
+++ src/main/python/Problem0003.py
@@ -0,0 +1,6 @@
+def solve(limit=4000000):
+    print("c")
+
+
+if __name__ == "__main__":
+    solve()
---
+++ src/main/python/Problem0005.py
@@ -0,0 +1,6 @@
+def solve(limit=4000000):
+    print("dsasdsa")
+
+
+if __name__ == "__main__":
+    solve()
