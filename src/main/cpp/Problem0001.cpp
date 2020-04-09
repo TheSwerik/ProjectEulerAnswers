298ff6f9d21fd8dd0a69646a29c448d29c23828b
TheSwerik <derswerik@gmail.com>
2020-04-09 11:07:44 +0200

    sneaky

--- src/main/cpp/Problem0001.cpp
+++ src/main/cpp/Problem0001.cpp
@@ -48,4 +48,20 @@ dissertation that compared the philosophies of Descartes and Newton. During that
 afternoon lessons from Johann Bernoulli, who quickly discovered his new pupil's incredible talent for mathematics.[
 10] At that time Euler's main studies included theology, Greek and Hebrew at his father's urging to become a
 pastor, but Bernoulli convinced his father that Leonhard was destined to become a great mathematician.
+
+Music Even when dealing with music, Euler's approach is mainly mathematical. His writings on music are not
+particularly numerous (a few hundred pages, in his total production of about thirty thousand pages),
+but they reflect an early preoccupation and one that did not leave him throughout his life.[54]
+
+A first point of Euler's musical theory is the definition of "genres", i.e. of possible divisions of the octave
+using the prime numbers 3 and 5. Euler describes 18 such genres, with the general definition 2mA, where A is the
+"exponent" of the genre (i.e. the sum of the exponents of 3 and 5) and 2m (where "m is an indefinite number,
+small or large, so long as the sounds are perceptible"[55]), expresses that the relation holds independently of the
+number of octaves concerned. The first genre, with A = 1, is the octave itself (or its duplicates); the second
+genre, 2m.3, is the octave divided by the fifth (fifth + fourth, C–G–C); the third genre is 2m.5, major third +
+minor sixth (C–E–C); the fourth is 2m.32, two-fourths and a tone (C–F–B♭–C); the fifth is 2m.3.5 (C–E–G–B–C); etc.
+Genres 12 (2m.33.5), 13 (2m.32.52) and 14 (2m.3.53) are corrected versions of the diatonic, chromatic and
+enharmonic, respectively, of the Ancients. Genre 18 (2m.33.52) is the "diatonico-chromatic", "used generally in all
+compositions",[56] and which turns out to be identical with the system described by Johann Mattheson.[57] Euler
+later envisaged the possibility of describing genres including the prime number 7.[58]
 */
