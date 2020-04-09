83a415463efbd1ec1df1801b81d2034da2517abd
TheSwerik <derswerik@gmail.com>
2020-04-09 11:08:40 +0200

    sneaky

--- src/main/cpp/Problem0001.cpp
+++ src/main/cpp/Problem0001.cpp
@@ -41,11 +41,4 @@ and a younger brother, Johann Heinrich.[9] Soon after the birth of Leonhard, the
 town of Riehen, where Leonhard spent most of his childhood. Paul was a friend of the Bernoulli family; Johann
 Bernoulli, then regarded as Europe's foremost mathematician, would eventually be the most important influence on
 young Leonhard.
-
-Euler's formal education started in Basel, where he was sent to live with his maternal grandmother. In 1720,
-at age thirteen, he enrolled at the University of Basel. In 1723, he received a Master of Philosophy with a
-dissertation that compared the philosophies of Descartes and Newton. During that time, he was receiving Saturday
-afternoon lessons from Johann Bernoulli, who quickly discovered his new pupil's incredible talent for mathematics.[
-10] At that time Euler's main studies included theology, Greek and Hebrew at his father's urging to become a
-pastor, but Bernoulli convinced his father that Leonhard was destined to become a great mathematician.
 */
