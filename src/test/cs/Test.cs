b0bc93788b2cfa16c69d8d6e25b2d932c828976e
TheSwerik <derswerik@gmail.com>
2020-05-25 10:21:41 +0200

    add code  for command line args

--- src/test/cs/Test.cs
+++ src/test/cs/Test.cs
@@ -9,7 +9,7 @@ namespace Euler.test.cs
         public static bool DoBenchmark = false;
         private const bool Release = true;
 
-        private static void Main()
+        private static void Main(string[] args)
         {
             Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
             if (!Release) Environment.CurrentDirectory = "..\\..\\..\\src";
@@ -19,26 +19,30 @@ namespace Euler.test.cs
                 return;
             }
 
-            while (true)
+            if (args.Length > 0) Start(args[0]);
+            while (args.Length == 0)
             {
                 Console.WriteLine("\nWhich do you want to try out?");
                 var input = Console.ReadLine();
                 if (input.Equals("0")) return;
+                Start(input);
+            }
+        }
 
-                //start Problems:
-                var path = "Euler.main.cs._0" + (input.Length < 3 ? '0' : input[0]) + ".Problem" +
-                           new string('0', 4 - input.Length) + input;
-                var problem = Type.GetType(path);
-                if (problem == null)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Cannot find the Class: " + path);
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Activator.CreateInstance(problem);
-                }
+        private static void Start(string input)
+        {
+            var path = "Euler.main.cs._0" + (input.Length < 3 ? '0' : input[0]) + ".Problem" +
+                       new string('0', 4 - input.Length) + input;
+            var problem = Type.GetType(path);
+            if (problem == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Cannot find the Class: " + path);
+                Console.ResetColor();
+            }
+            else
+            {
+                Activator.CreateInstance(problem);
             }
         }
     }
